f9c73ae27ef6a9de5cb47a2563073f56717e23ca
MuhammedKhamis <muhammedessam10@gmail.com>
2017-10-20 22:28:32 +0200

    :unicorn:

--- main.c
+++ main.c
@@ -10,30 +10,28 @@
 #include <string.h>
 #include <stdio.h>
 
-const char* defaultSrc1= "a.txt";
-const char* defaultSrc2= "b.txt";
-const char* defaultDest= "c";
+const char* defaultValues[]  = {"a.txt","b.txt","c"};
+
+void setValue(char* dest,char* argv[],int argc, int i){
+
+    if(argc > i){
+        strcat(dest,argv[i]);
+    }else{
+        strcat(dest,defaultValues[i-1]);
+    }
+}
 
 
 int main(int argc , char* argv[]) {
-    char src1[MAX_FILE_NAME_SIZE]={'\0'};
-    char src2[MAX_FILE_NAME_SIZE]={'\0'};
-    char dest1[MAX_FILE_NAME_SIZE]={'\0'};
-    char dest2[MAX_FILE_NAME_SIZE]={'\0'};
-    if(argc > 1){
-        strcat(src1,argv[1]);
-        strcat(src2,argv[2]);
-        strcat(dest1,argv[3]);
-        strcat(dest2,argv[3]);
-    }else{
-        strcat(src1,defaultSrc1);
-        strcat(src2,defaultSrc2);
-        strcat(dest1,defaultDest);
-        strcat(dest2,defaultDest);
+    char val[4][MAX_FILE_NAME_SIZE]={'\0'};
+    int i = 1;
+    for(;i<4;i++){
+     setValue(val[i-1],argv,argc,i);
     }
+    setValue(val[3],argv,argc,3);
     Thread_info* info = malloc(sizeof(*info));
-    info->params.mat1= readFromFile(src1);
-    info->params.mat2= readFromFile(src2);
+    info->params.mat1= readFromFile(val[0]);
+    info->params.mat2= readFromFile(val[1]);
     //
         if(info->params.mat2->rows != info->params.mat1->cols){
             fprintf(stderr,"Mismatch in the sizes of the two input arrays\n");
@@ -44,9 +42,9 @@ int main(int argc , char* argv[]) {
     printMatrix(info->params.mat1);
     ////////////////////////////////////////////////////
     if(!calculateTime(info,&threadByCell))
-        writeIntoFile(strcat(dest1,"_1"),info->res);
+        writeIntoFile(strcat(val[2],"_1"),info->res);
     if(!calculateTime(info,&threadByRow))
-        writeIntoFile(strcat(dest2,"_2"),info->res);
+        writeIntoFile(strcat(val[3],"_2"),info->res);
     //calculateTime(info,&threadByMatrix);
     freeThread_info(info);
 }
